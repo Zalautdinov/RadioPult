5258eb729c9e44faad78aec3c8c355a871a39b13
Zalautdinov <50925258+Zalautdinov@users.noreply.github.com>
2021-09-25 02:36:49 +0300

    Add files via upload

--- RadioPult/src/radiopult.cpp
+++ RadioPult/src/radiopult.cpp
@@ -1,11 +1,46 @@
 #include <radiopult.h>
 
-void Radio_Pult::begin(uint8_t netmod)
+void Radio_Pult::begin(uint8_t netmod, byte ce, byte cs)
 {
     net_mod = netmod;
+    if (net_mod == 0) // чтенире данных с радиомодуля Wifi
+    {
+        IPAddress local_IP(192, 168, 4, 1); // не определнн IP адрес
+        IPAddress gateway(192, 168, 4, 1);
+        IPAddress subnet(255, 255, 255, 0);
+        IPAddress dns(8, 8, 8, 8);
+
+        for (byte n = 0; n < 19; n++)
+        {
+            ssid[n + 1] = Name[n];
+        }
+        WiFi.mode(WIFI_AP);
+        WiFi.softAP(ssid, password);
+        udp.begin(udp_port);
+        t1 = millis();
+    }
+
+    if (net_mod == 1) // чтенире данных с радиомодуля NRF24
+    {
+        if (ce == 0 && cs == 0)
+        {
+
+#if defined(ESP8266) || defined(ARDUINO_ESP8266_NODEMCU)
+            ce = D4;
+            cs = D8;
+#endif
+#if defined(ESP32)
+            ce = 4;
+            cs = 5;
+#endif
+        }
+        radio.begin(ce, cs);
+        init_nrf();
+        t1 = millis();
+    }
 }
 void Radio_Pult::servo(byte port, int t)
-{ 
+{
     pinMode(port, OUTPUT);
     t = map(t, 0, 100, 540, 2400);
     digitalWrite(port, 1);
@@ -14,11 +49,137 @@ void Radio_Pult::servo(byte port, int t)
     delayMicroseconds(20000 - t);
 }
 
-void Radio_Pult::ReadData()
+bool Radio_Pult::Priem()
 {
-    if (net_mod == 1) // чтенире данных с радиомодуля NRF24
+    if (millis() - t1 > 2000)
+        for (byte n = 0; n < sizeof(b_read); n++)
+        {
+            //b_read[n] = 0;
+        }
+    switch (net_mod)
     {
+    case RP_WIFI:
+    {
+        int packetSize = udp.parsePacket();
+        if (packetSize)
+        {
+            udp.read(b_read, 32);
+            t1 = millis();
+            return true;
+        }
+        return false;
+        break;
+    }
+    case RP_NRF24:
+    {
+        if (_connect) // если связь установлена
+        {
+            // установить буфер ответа если он включен
+            if (radio.available()) // Слушаем канал связи
+            {
+                t1 = millis();
+                radio.read(&b_read, sizeof(b_read));
+                // загрузить буфер ответа если установлен флаг
+                return true;
+            }
+        }
+        return false;
+        break;
+    }
+
+    default:
+        return false;
     }
 }
+void Radio_Pult::init_nrf() // поиск каналоа и ожидание подключения
+{
+    uint8_t pip0[6] = "setup"; // труба для настройки
 
+    //radio.begin(); // Инициируем работу модуля nRF24L01+.
+    radio.setAutoAck(true);
+    radio.setDataRate(RF24_1MBPS);  // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек.
+    radio.setPALevel(RF24_PA_HIGH); // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm).
+    radio.openReadingPipe(1, pip0);
+    radio.powerUp();
+
+    byte ch = 1;
+    while (ch <= 125)
+    {
+        int scanal = 0;
+        int i = 100;
+
+        while (i--)
+        {
+            radio.setChannel(ch);
+            radio.startListening();
+            delayMicroseconds(200);
+            radio.stopListening();
+            if (radio.testCarrier())
+                scanal++;
+        }
+        //Serial.println(String(ch) + " - " + String(scanal));
+
+        if (scanal < 2) // нашел чистый канал
+        {
+            break;
+        }
+        ch++;
+    }
+    //  формирует название машинки для пульта
+    for (byte n = 0; n < 19; n++)
+    {
+        s[n + 1] = Name[n];
+    }
+
+    //Serial.println(ch);
+    radio.setAutoAck(true);
+    //radio.setRetries(1, 15);
+    radio.enableAckPayload();
+    radio.openReadingPipe(1, pip0);
+    radio.startListening(); // включаем прослушку
+    //radio.openWritingPipe(pip0);
+    //radio.setChannel(ch); // устанавливаем очередной канал работы модуля
+    radio.writeAckPayload(1, &s, sizeof(s)); // Помещаем данные всего массива ackData в буфер FIFO для их отправки на следующее получение данных от передатчика на 1 трубе.
+    //Serial.println("Поиск пульта");
+
+    for (;;)
+    {
+
+        if (radio.available())
+        {
+            // Если в буфере приёма имеются принятые данные от передатчика, то ...
+            char msg[32] = "";
+            radio.read(&msg, sizeof(msg));           // Читаем данные из буфера приёма в массив myData указывая сколько всего байт может поместиться в массив.
+            radio.writeAckPayload(1, &s, sizeof(s)); // Помещаем данные всего массива ackData в буфер FIFO для их отправки на следующее получение данных от передатчика на 1 трубе.
+
+            //Serial.println(String(radio.getChannel()) + " - " + msg);
+
+            if (String(msg).startsWith("#pip#"))
+            {
+                pip[0] = msg[5];
+                pip[1] = msg[6];
+                pip[2] = msg[7];
+                pip[3] = msg[8];
+                pip[4] = msg[9];
+
+                set_ch = radio.getChannel();
+                radio.openReadingPipe(1, pip);
+                _connect = true;
+                //Сохранить данные в памяти
+                //write_eprom();
+                return;
+            }
+        }
+    }
+}
+void Radio_Pult::PowerControl(byte *h, byte min_value)
+{
+    if (*h == min_value)
+        power_control = 1;
+
+    if (power_control == 0)
+    {
+        *h = 0;
+    }
+}
 Radio_Pult RadioPult = Radio_Pult();
--- RadioPult/src/radiopult.h
+++ RadioPult/src/radiopult.h
@@ -2,6 +2,14 @@
 #include <Arduino.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#include <WiFiUdp.h>
+
+#if defined(ESP8266) || defined(ARDUINO_ESP8266_NODEMCU)
+#include <ESP8266WiFi.h>
+#endif
+#if defined(ESP32)
+#include <WiFi.h>
+#endif
 
 #define RP_WIFI 0
 #define RP_NRF24 1
@@ -9,15 +17,54 @@
 class Radio_Pult
 {
 public:
-    void begin(uint8_t netmod = RP_WIFI);
+    void begin(uint8_t netmod = RP_WIFI, byte ce = 0, byte cs = 0);
     void servo(byte port, int t = 50);
-    void ReadData();
+    bool Priem();
+    void init_nrf();
+    void PowerControl(byte *h, byte min_value = 0);
+    String Name = "";
+    byte *p_mode = b_read;      // код описания протокола 0 - прием данных управления с пульта
+    byte *x1 = b_read + 1;      // левый джойстик ось *X1 УКАЗАТЕЛЬ
+    byte *y1 = b_read + 2;      // левый джойстик ось *Y1 УКАЗАТЕЛЬ
+    byte *x2 = b_read + 3;      // првый джойстик ось *X2 УКАЗАТЕЛЬ
+    byte *y2 = b_read + 4;      // правый джойстик ось *Y2 УКАЗАТЕЛЬ
+    byte *key_xy1 = b_read + 5; // кнопка левая "ОГОНЬ"
+    byte *key_xy2 = b_read + 6; // кнопка правая "ОГОНЬ"
+    byte *key1 = b_read + 7;    // левая кнопка на панели
+    byte *key2 = b_read + 8;    // средняя кнопка на понели
+    byte *key3 = b_read + 9;    // правая кнопка на панели
+    byte *L_fire = b_read + 10; // кнопка правого джойстика
+    byte *R_fire = b_read + 11; // кнопка левого джойстика
+    int udp_port = 4220;        // Порт подключения по UDP
+    byte power_control = 0;
 
 private:
-    byte ch = 255; // Канал для подключения NRF модуля
+    //char name[19] = "Кораблик"; // Имя устройства выводится в меню пульта управления
+    byte set_ch = 255;          // Канал для подключения NRF модуля
     byte net_mod = RP_WIFI;
-byte g;
-    //uint8_t pip[6];            // труба для связи присылает пульт
-    //uint8_t pip0[6] = "setup"; // труба для настройки
+    bool _connect = false; // состояние связи
+    uint8_t pip[6];        // труба для связи присылает пульт
+    char s[20] = "#";
+    RF24 radio;
+    WiFiUDP udp;
+    unsigned long t1 = 100000;
+    uint8_t b_read[32]; // буфер для приема данных
+
+    /* структура пакета данных от пульта управления
+        msg[0]  - назначение протокола данных 0 - данные с пульта
+        msg[1]  - левый джойстик X1
+        msg[2]  - левый джойстик Y1
+        msg[3]  - правый джойстик X1
+        msg[4]  - правый джойстик Y1
+        msg[5]  - левый огонь
+        msg[6]  - правый огонь
+        msg[7]  - кнопка слева
+        msg[8]  - кнопка по центру 
+        msg[9]  - кнопка справа
+        msg[10] - кнопка правого джойстика
+        msg[11] - кнопка левого джойстика
+    */
+    char ssid[20] = "-";               //Ship Kater";  //Имя точки доступа WIFI
+    const char *password = "85371571"; //пароль точки доступа WIFI
 };
 extern Radio_Pult RadioPult;
